42ba289ca865a24b8fcd701b81b7537e7ee981ad
Meiir Akhmetov HP <meir.akhmetov@gmail.com>
2022-08-02 12:16:44 +0600

    1_3_HW0_option

--- src/main/java/kz/meiir/topjava/util/MealsUtil.java
+++ src/main/java/kz/meiir/topjava/util/MealsUtil.java
@@ -16,6 +16,7 @@ import java.util.stream.Collectors;
  * @author Meiir Akhmetov on 01.08.2022
  */
 public class MealsUtil {
+    private static final int DEFAULT_CALORIES_PER_DAY = 2000;
     public static void main(String[] args){
         List<Meal> meals = Arrays.asList(
                 new Meal(LocalDateTime.of(2022, Month.JULY,30,10,0),"Завтрак", 500),
@@ -26,41 +27,76 @@ public class MealsUtil {
                 new Meal(LocalDateTime.of(2022, Month.JULY,31,13,0),"Обед", 500),
                 new Meal(LocalDateTime.of(2022, Month.JULY,31,20,0),"Ужин", 410)
         );
-        List<MealTo> mealsWithExcess = getFilteredWithExcess(meals, LocalTime.of(7,0), LocalTime.of(12,0),2000);
-        mealsWithExcess.forEach(System.out::println);
+        LocalTime startTime = LocalTime.of(7,0);
+        LocalTime endTime = LocalTime.of(12,0);
 
-        System.out.println(getFilteredWithExcessByCycle(meals, LocalTime.of(7,0),LocalTime.of(12,0),2000));
+        List<MealTo> mealTo = getFiltered(meals, startTime, endTime, DEFAULT_CALORIES_PER_DAY);
+        mealTo.forEach(System.out::println);
+        System.out.println("--------");
+
+        getFilteredByCycle(meals, startTime, endTime, DEFAULT_CALORIES_PER_DAY).forEach(System.out::println);
+        System.out.println("--------");
+        System.out.println(getFilteredByRecursion(meals, startTime, endTime, DEFAULT_CALORIES_PER_DAY));
+
+
+
+
 
     }
 
-    public static List<MealTo> getFilteredWithExcessByCycle(List<Meal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        final Map<LocalDate, Integer> caloriesSumByDate = new HashMap<>();
-        meals.forEach(meal -> caloriesSumByDate.merge(meal.getDate(), meal.getCalories(), Integer::sum));
 
-        final List<MealTo> mealWithExcess = new ArrayList<>();
-        meals.forEach(meal ->{
-                if(TimeUtil.isBetweenInclusive(meal.getTime(), startTime,endTime)) {
-                    mealWithExcess.add(creatWithExcess(meal, caloriesSumByDate.get(meal.getDate()) > caloriesPerDay));
-                }
-        });
-        return mealWithExcess;
-    }
 
-    private static MealTo creatWithExcess(Meal meal, boolean excess){
-        return new MealTo(meal.getDateTime(),meal.getDescription(), meal.getCalories(),excess);
-    }
-
-    public static List<MealTo> getFilteredWithExcess(List<Meal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+    public static List<MealTo> getFiltered(List<Meal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         Map<LocalDate, Integer> caloriesSumByDate = meals.stream()
                 .collect(
                         Collectors.groupingBy(Meal::getDate, Collectors.summingInt(Meal::getCalories))
                 );
         return meals.stream()
-                .filter(um -> TimeUtil.isBetweenInclusive(um.getTime(),startTime,endTime))
+                .filter(um -> TimeUtil.isBetween(um.getTime(),startTime,endTime))
                 .map(um -> new MealTo(um.getDateTime(),um.getDescription(),um.getCalories(),
                         caloriesSumByDate.get(um.getDate()) > caloriesPerDay))
                 .collect(Collectors.toList());
     }
+
+
+
+    public static List<MealTo> getFilteredByCycle(List<Meal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+        final Map<LocalDate, Integer> caloriesSumByDate = new HashMap<>();
+        meals.forEach(meal -> caloriesSumByDate.merge(meal.getDate(), meal.getCalories(), Integer::sum));
+
+        final List<MealTo> mealsTo = new ArrayList<>();
+        meals.forEach(meal ->{
+                if(TimeUtil.isBetween(meal.getTime(), startTime,endTime)) {
+                    mealsTo.add(createTo(meal, caloriesSumByDate.get(meal.getDate()) > caloriesPerDay));
+                }
+        });
+        return mealsTo;
+    }
+
+    private static List<MealTo> getFilteredByRecursion(List<Meal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+        List<MealTo> result = new ArrayList<>();
+        filterWithRecursion(new LinkedList<>(meals),startTime,endTime,caloriesPerDay, new HashMap<>(), result);
+        return result;
+    }
+
+    private static void filterWithRecursion(LinkedList<Meal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay,
+                                            Map<LocalDate, Integer> dailyCaloriesMap, List<MealTo> result) {
+        if(meals.isEmpty()) return;
+        Meal meal = meals.pop();
+        dailyCaloriesMap.merge(meal.getDate(),meal.getCalories(),Integer::sum);
+        filterWithRecursion(meals,startTime,endTime,caloriesPerDay,dailyCaloriesMap,result);
+        if(TimeUtil.isBetween(meal.getTime(),startTime,endTime)){
+            result.add(createTo(meal, dailyCaloriesMap.get(meal.getDate())>caloriesPerDay));
+        }
+    }
+
+
+
+    private static MealTo createTo(Meal meal, boolean excess){
+        return new MealTo(meal.getDateTime(),meal.getDescription(), meal.getCalories(),excess);
+    }
+
+
 
 
 }
--- src/main/java/kz/meiir/topjava/util/TimeUtil.java
+++ src/main/java/kz/meiir/topjava/util/TimeUtil.java
@@ -6,7 +6,7 @@ import java.time.LocalTime;
  * @author Meiir Akhmetov on 01.08.2022
  */
 public class TimeUtil {
-    public static boolean isBetweenInclusive(LocalTime lt, LocalTime startTime, LocalTime endTime){
+    public static boolean isBetween(LocalTime lt, LocalTime startTime, LocalTime endTime){
         return lt.compareTo(startTime)>=0 && lt.compareTo(endTime) <=0;
     }
 }
