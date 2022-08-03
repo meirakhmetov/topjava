ac134cfea1b8a47c973d66a5e6f8f430884380fd
Meiir Akhmetov HP <meir.akhmetov@gmail.com>
2022-08-03 12:14:45 +0600

    1_7_final

---
+++ src/main/java/kz/meiir/topjava/web/UserServlet.java
@@ -0,0 +1,25 @@
+package kz.meiir.topjava.web;
+
+import org.slf4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * @author Meiir Akhmetov on 02.08.2022
+ */
+public class UserServlet extends HttpServlet {
+    private static final Logger LOG = getLogger(UserServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LOG.debug("redirect to users");
+       // request.getRequestDispatcher("users.jsp").forward(request,response);
+        response.sendRedirect("users.jsp");
+    }
+}
---
+++ src/main/webapp/users.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java"%>
+<html>
+<head>
+    <title>Users list</title>
+</head>
+<body>
+<h3><a href="index.html">Home</a> </h3>
+<hr>
+<h2>Users LIST</h2>
+</body>
+</html>
